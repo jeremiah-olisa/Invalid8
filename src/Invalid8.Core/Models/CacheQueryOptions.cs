8a382c69f357b26fd4a5b57a94f9a321dd257ef1
Jeremiah Olisa <jeremiaholisa453@gmail.com>
2025-06-23 18:07:18 +0100

    Add new properties to CacheQueryOptions class
    Updated the `CacheQueryOptions` class in the `Invalid8.Core` namespace to include new properties: `StaleTime`, `CacheTime`, `RetryCount`, `RetryDelay`, and `EnableBackgroundRefetch`. Default values have been assigned for each property to enhance caching behavior and control.

--- src/Invalid8.Core/Models/CacheQueryOptions.cs
+++ src/Invalid8.Core/Models/CacheQueryOptions.cs
@@ -2,5 +2,9 @@
 
 public class CacheQueryOptions
 {
-
+    public TimeSpan? StaleTime { get; set; } = TimeSpan.FromMinutes(5);
+    public TimeSpan? CacheTime { get; set; } = TimeSpan.FromMinutes(30);
+    public int RetryCount { get; set; } = 3;
+    public TimeSpan RetryDelay { get; set; } = TimeSpan.FromSeconds(1);
+    public bool EnableBackgroundRefetch { get; set; } = true;
 }
